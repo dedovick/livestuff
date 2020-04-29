18880dff577e645969499d3dce59a609b6af7984
gstmachado <gabrielsmachado@gmail.com>
2020-04-29 20:44:55 -0300

    Migracao heroku - google

--- app/server.js
+++ app/server.js
@@ -1,18 +1,16 @@
 //const http = require('http')
-const port = 3000
-const ip = 'localhost'
+const port = process.env.PORT || 3000
+const ip = '0.0.0.0'
 const low = require('lowdb')
 const FileAsync = require('lowdb/adapters/FileAsync')
 const express = require('express')
+const cors = require('cors')
 const bodyParser = require('body-parser')
-const schedule = require('node-schedule');
-const axios = require('axios');
 
 // Create server
 const app = express()
+app.use(cors())
 app.use(bodyParser.json())
-
-apiKey = 'AIzaSyDtbEKEUxgJb1TeugAVKTFIuTJmYocnvbE';
 
 // Create database instance and start server
 const adapter = new FileAsync('db.json')
@@ -21,40 +19,88 @@ low(adapter)
     // Routes
     // GET /events
     app.get('/events', (req, res) => {
-      console.log(req.params.id);
-      //const post = db.get('posts').find({id : req.params.id}).value();
-      const post = db.get('event').value();
+      const today = new Date();
+      var todayString = '';
+      const year = today.getFullYear();
+      const month = today.getMonth() + 1;
+      const date = today.getDate();
+      if (month > 9) {
+        todayString = year + '-' + month;
+      } else {
+        todayString = year + '-0' + month;
+      }
+      if (date > 9) {
+        todayString += '-' + date;
+      } else {
+        todayString += '-0' + date;
+      }
+      console.log('AAAAAAAA: ' + todayString);
+      const post = db.get('event').sortBy('data').value();
       console.log(post);
+      res.send(post)
+    })
+
+    // GET /events by date
+    app.get('/events/:data', (req, res) => {
+      const post = db.get('event').filter({ data: req.params.data}).value();
+      console.log(post);
+      res.send(post)
+    })
+    
+    // GET /events by idchannel
+    app.get('/channels/events/:idyoutube', (req, res) => {
+      const post = db.get('event').filter({ idYoutube: req.params.idyoutube}).value();
       res.send(post)
     })
 
     // GET /channels
     app.get('/channels', (req, res) => {
-      console.log(req.params.id);
       //const post = db.get('posts').find({id : req.params.id}).value();
-      const post = db.get('channel').value();
+      const post = db.get('channel').sortBy('nome').value();
       console.log(post);
       res.send(post)
     })
-
-    var j = schedule.scheduleJob('* * * * *', function(){
-      console.log('Entrou na rotina')
-      const events = db.get('event').filter({data : '2020-04-25'}).value();
-      events.forEach(event => {
-        console.log('Encontrou eventos: ' + event)
-        axios.get('https://www.googleapis.com/youtube/v3/videos?part=snippet&key=' + apiKey + '&id=' + event.videoId)
-          .then(response => {
-            console.log(response.data.items);
-          })
-          .catch(error => {
-            console.log(error);
-          });
-      });
-    });
+	
+	// POST /events
+    app.post('/channels', (req, res) => {
+      console.log(req.body);
+      db.get('channel')
+		.push(req.body)
+		.last()
+		.assign({ id: Date.now().toString() })
+        .write()
+        .then(post => res.send(post));
+    })
+	
+	// POST /events
+    app.post('/events', (req, res) => {
+      console.log(req.body);
+      db.get('event')
+		.push(req.body)
+		.last()
+		.assign({ id: Date.now().toString() })
+        .write()
+        .then(post => res.send(post));
+    })
 
     // Set db default values
     return db.defaults({ posts: [] }).write()
   })
   .then(() => {
-    app.listen(3000, () => console.log('listening on port 3000'))
+    app.listen(port, ip, () => console.log('listening on port 3000'))
   })
+/*const server = http.createServer((req, res) => {
+
+  const responses = []
+  responses['/'] = '<h1>Home</h1>'
+  responses['/shows'] = '<h1>Shows</h1>'
+  responses['/search'] = '<h1>Search</h1>'
+  responses['/naoExiste'] = '<h1>URL sem resposta definida!</h1>'
+
+  res.end(responses[req.url] || responses['/naoExiste'])
+})
+
+server.listen(port, ip, () => {
+  console.log(`Servidor rodando em http://${ip}:${port}`)
+  console.log('Para derrubar o servidor: ctrl + c');
+})*/
